46ac50bdbcc151b3574387c1b5f03d53856b8026
omar ashraf <124735221+Omar-Ashraf7215@users.noreply.github.com>
2025-08-10 01:52:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿namespace day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Flowcontrol
+            //Console.WriteLine("plz enter your Age");
+            //int age= int.Parse(Console.ReadLine());
+            //if (age >= 6 && age <= 18) 
+            //{
+            //    Console.WriteLine("Student at School");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not Student at school");
+            //}
+            //Console.WriteLine("plz enter your degreee");
+            //int degreee = int.Parse(Console.ReadLine());
+            //if (degreee >= 50 && degreee <= 75)
+            //{
+            //    Console.WriteLine("Good");
+            //}
+            //else if (degreee >= 60 && degreee <= 75)
+            //{
+            //    Console.WriteLine("Very Good");
+            //}
+            //else if (degreee >= 75 && degreee <= 100)
+            //{
+            //    Console.WriteLine("EX");
+            //}
+            //else {
+            //    Console.WriteLine("FAIL");
+            //}
+
+            //nested if 
+            //Console.WriteLine("plz enter your name ");
+            //string name = Console.ReadLine();
+            //if (name == "")
+            //{
+            //    Console.WriteLine("Re enter your name ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Enter your age");
+            //    int age = int.Parse(Console.ReadLine());
+            //    if (age > 18)
+            //    {
+            //        Console.WriteLine("invaild");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("vaild");
+            //    }
+            //}
+            //switch
+            //string gender;
+            //do
+            //{
+            //    Console.WriteLine("Enter your gender ");
+            //    gender = Console.ReadLine();
+            //    switch (gender)
+            //    {
+            //        case "male":
+            //            Console.WriteLine("enter your name ");
+            //            Console.WriteLine("Enter your age");
+            //            Console.WriteLine("you complete army");
+            //            break;
+            //        case "female":
+            //            Console.WriteLine("Re enter your name ");
+            //            Console.WriteLine("Enter your age");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Re choise vaild gender (male or female)");
+            //            break;
+            //    }
+            //}
+            //while (gender != "male" || gender != "female");
+            #endregion
+            #region looping
+            //for
+            //for (int i = 0; i < 50; i++)
+            //{
+            //    Console.WriteLine("Hello From BIS");
+            //}
+            //while
+            //Console.WriteLine("enter inital value");
+            //int value = int.Parse(Console.ReadLine());
+            //while (value < 100) 
+            //{
+            //    Console.WriteLine("vaild");
+            //    Console.WriteLine("enter inital value");
+            //    value = int.Parse(Console.ReadLine());
+            //}
+            //do-while
+            //int age;
+            //do
+            //{
+            //    Console.WriteLine("enter your age ");
+            //    age = int.Parse(Console.ReadLine());
+            //} while (age < 100);
+            #endregion
+            #region function
+            //void display()
+            //{
+            //    Console.WriteLine("hello from BIS");
+            //}
+            //display();
+            //void display(string name)
+            //{
+            //    Console.WriteLine($"hello {name}");
+            //}
+            ////display("Omar");
+            //Console.WriteLine($"enter your name ");
+            //string name=Console.ReadLine();
+            //display(name);
+            //int sum(int num1, int num2)
+            //{
+            //    return num1 + num2;
+            //}
+            //Console.WriteLine($"plz enter number 1");
+            //int number1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"plz enter number 2");
+            //int number2 = int.Parse(Console.ReadLine());
+            //sum(5, 10);
+            //Console.WriteLine($"sum :{sum(number1, number2)}");
+            #endregion
+        }
+    }
+}
